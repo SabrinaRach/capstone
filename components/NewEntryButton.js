673f5b7ef85d4a2acbc6c925978c48d415206941
Sabrina <sabrinarach.sr@gmail.com>
2026-09-04 16:08:39 +0200

    change text to plus icon

--- components/NewEntryButton.js
+++ components/NewEntryButton.js
@@ -11,7 +11,21 @@ export default function NewEntryButton() {
         onClick={() => setIsOpen(true)}
         className="rounded-lg bg-primary-500 px-4 py-2 font-medium text-background hover:bg-primary-700"
       >
-        New Entry
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="var(--accent-500)"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-plus-icon lucide-plus"
+        >
+          <path d="M5 12h14" />
+          <path d="M12 5v14" />
+        </svg>
       </button>
 
       {isOpen && <EntryModal onClose={() => setIsOpen(false)} />}
