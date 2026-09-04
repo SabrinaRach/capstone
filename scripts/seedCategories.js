7c0031b33ded5ca93c6f266e3a7f25890cc44fce
Sabrina <sabrinarach.sr@gmail.com>
2026-09-04 10:11:19 +0200

    add seed system categories, add colors for categories to global.css

---
+++ scripts/seedCategories.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import Category from "../db/models/Category.js";
+
+dotenv.config({ path: ".env.local" });
+
+const { default: dbConnect } = await import("../db/connect.js");
+
+const systemCategories = [
+  {
+    name: "Recipes",
+    slug: "recipes",
+    color: "#6B8F71",
+    isSystem: true,
+  },
+  {
+    name: "How-to / Instructions",
+    slug: "how-to-instructions",
+    color: "#7C6F8F",
+    isSystem: true,
+  },
+  {
+    name: "Guides",
+    slug: "guides",
+    color: "#B08A4A",
+    isSystem: true,
+  },
+  {
+    name: "Other / Not assigned",
+    slug: "other",
+    color: "#7D8587",
+    isSystem: true,
+  },
+];
+
+async function seedCategories() {
+  await dbConnect();
+
+  for (const category of systemCategories) {
+    await Category.updateOne(
+      { slug: category.slug },
+      { $setOnInsert: category },
+      { upsert: true },
+    );
+  }
+
+  console.log("System categories seeded.");
+
+  await mongoose.connection.close();
+}
+
+seedCategories().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
