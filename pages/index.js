6bccea482e389a5f5a4be2063e0dc264ab971d2f
Sabrina <sabrinarach.sr@gmail.com>
2026-09-08 16:12:57 +0200

    put animation in center

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import AnimationVortex from "@/components/AnimationVortex";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-background dark:bg-foreground sm:items-start">
+    <main className="flex min-h-screen w-full items-center justify-center bg-background dark:bg-foreground sm:items-start">
       <AnimationVortex />
     </main>
   );
