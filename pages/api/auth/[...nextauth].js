2708b815a880d4bb50618308680fc17cfe588598
Sabrina <sabrinarach.sr@gmail.com>
2026-09-14 13:54:41 +0200

    Add NextAuth configuration with GitHub and credentials providers

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    process.env.VERCEL_ENV === "preview"
+      ? CredentialsProvider({
+          name: "credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "username",
+            },
+            password: {
+              label: "Password",
+              type: "password",
+            },
+          },
+          async authorize(credentials) {
+            if (
+              credentials.username === "fisch" &&
+              credentials.password === "fisch"
+            ) {
+              return {
+                name: "Neuer Fisch",
+                email: "test@example.com",
+                id: "a1b2c3d4",
+              };
+            }
+
+            return null;
+          },
+        })
+      : GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+        }),
+  ],
+};
+
+export default NextAuth(authOptions);
